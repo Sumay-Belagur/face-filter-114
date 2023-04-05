9ce02255054220dbca9c28550d079c41d6270244
Sumay-Belagur <75619330+Sumay-Belagur@users.noreply.github.com>
2023-04-05 19:16:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload() {
+
+}
+
+
+function setup() {
+canvas= createCanvas(300, 300);
+canvas.position(540, 250)
+}
+
+function draw() {
+
+}
+
+function snapshot() {
+    save("myPhoto");
+}
